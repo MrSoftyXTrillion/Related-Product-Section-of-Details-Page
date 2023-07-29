dea13b4f6c2314c102152c0c1a69a10595bbe984
Mr.SoftyXTrillion <khaledbinnasir1714412140@gmail.com>
2023-07-29 17:14:15 +0600

    Design and Responsive Done

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$('.slickSliderMain').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '.leftArrow',
+    nextArrow: '.rightArrow',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
